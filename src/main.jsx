2968b80d5c2b572c6eb890b6b1da7566035c2e58
Matheus Cantiere <matheus.cantieree@gmail.com>
2024-09-25 18:30:36 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Home from './pages/Home';
-import DetalhesFilmes from './pages/MovieDetailPage';
-import ListaFilmes from './pages/MovieListPage';
-import Genero from './pages/GenreListPage';
-import GeneroFilmes from './pages/MoviesByGenrePage';
 
 
 const router = createBrowserRouter([
@@ -16,10 +12,6 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'detalhes/:id', element: <DetalhesFilmes /> },
-      { path: 'filmes', element: <ListaFilmes /> },
-      { path: 'generofilme/:id', element: <GeneroFilmes /> },
-      { path: 'generos', element: <Genero /> },
     ],
   },
 ]);
---
+++ src/pages/Loja.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+    return (
+        <main className="bg-black text-white">
+        {/* Hero Section */}
+
+        {/* Jogos Section */}
+        <section className="jogos text-center py-5" id="jogos">
+
+        </section>
+
+        {/* Loja Section */}
+        <section className="loja text-center py-5" id="loja">
+
+        </section>
+
+        </main>
+    );
+}
